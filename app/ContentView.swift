37a9dee804aae0c7fe77d180b70aca8539f89143
Naoto Ono <55777215+naotono@users.noreply.github.com>
2025-03-28 13:47:47 +0900

    Initial Commit

---
+++ app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by cowtaro3 on 2025/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by cowtaro3 on 2025/03/28.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
